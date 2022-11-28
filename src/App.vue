8a04e3264039be839e55c12da7dd6309f76e5f06
star <sergrom12@yandex.ru>
2022-11-28 13:16:34 +0400

    1)  10. Создаем проект на vue3, клиентская часть интернет магазин

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to="/" class="mr-3">Some</router-link>
+    <router-link to="/" class="mr-3">Else</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+</style>
